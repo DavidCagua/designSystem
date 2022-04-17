d6d8c75c4e3cb2680b64f59c241e9f35c5567302
DavidCagua <felipe0160@gmail.com>
2022-04-17 15:27:43 -0500

    design system with playgrounds

---
+++ playgrounds/react/src/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { Button } from "@ds/react";
+import "@ds/scss/lib/Button.css";
+
+ReactDOM.render(<Button label="example" />, document.querySelector("#root"));
